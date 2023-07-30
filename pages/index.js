7153992975a1964bd03130faad2528300175e1b8
Spencer Saunders <SpencerSaunders@u.boisestate.edu>
2023-07-30 12:24:00 -0600

    Added button for loading more coins

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import { fetchMarketInfo } from "@/clients/CoinGeckoClient";
 import CryptoMarketTable from "@/src/components/CryptoMarketTable";
+import { Button } from "@mui/material";
 
 const CryptoMarketWatch = (props) => {
   const { marketInfo } = props;
 
   const [searchBarValue, setSearchBarValue] = useState("");
+  const [displayAmount, setDisplayAmount] = useState(50);
+  const [coinMarketInfo, setCoinMarketInfo] = useState(marketInfo);
+
+  const loadAdditionalMarketInfo = async () => {
+    // CoinGecko API can load up to 250 coins
+    if (displayAmount < 250) {
+      const numberOfCoinsToLoad = displayAmount + 50;
+      const response = await fetchMarketInfo(numberOfCoinsToLoad);
+      setDisplayAmount(displayAmount + 50);
+      setCoinMarketInfo(response);
+    }
+  };
 
   return (
     <main>
@@ -14,7 +27,17 @@ const CryptoMarketWatch = (props) => {
         To view more details for a specific coin, click on the name of a
         specific coin.
       </p>
-      <CryptoMarketTable marketInfo={marketInfo} searchTerm={searchBarValue} />
+      <CryptoMarketTable
+        marketInfo={coinMarketInfo}
+        searchTerm={searchBarValue}
+      />
+      <Button
+        className="load-more-coins-button"
+        onClick={() => loadAdditionalMarketInfo()}
+        variant="contained"
+      >
+        Load More Coins
+      </Button>
     </main>
   );
 };
